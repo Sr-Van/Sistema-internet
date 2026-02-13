91d75428ceae435baae7a074e1ea093c831f0451
Sr-Van <vanderson.dev1@gmail.com>
2026-02-13 11:48:22 -0300

    feat: create routes for each section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,31 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'ordem-de-servico',
+    loadComponent: () =>
+      import('./pages/os/os.component').then((m) => m.OsComponent),
+  },
+  {
+    path: 'clients',
+    loadComponent: () =>
+      import('./pages/clients/clients.component').then(
+        (m) => m.ClientsComponent,
+      ),
+  },
+  {
+    path: 'technicians',
+    loadComponent: () =>
+      import('./pages/technicians/technicians.component').then(
+        (m) => m.TechniciansComponent,
+      ),
+  },
+  {
+    path: 'profile',
+    loadComponent: () => {
+      return import('./pages/profile/profile.component').then(
+        (m) => m.ProfileComponent,
+      );
+    },
+  },
 ];
