6d3048b854541e329419800fe848a286112b42cf
Sr-Van <vanderson.dev1@gmail.com>
2026-02-13 11:50:38 -0300

    feat: create each page to load the components with routes

---
+++ src/app/pages/os/os.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-os',
+  standalone: true,
+  imports: [],
+  templateUrl: './os.component.html',
+  styleUrl: './os.component.css'
+})
+export class OsComponent {
+
+}
