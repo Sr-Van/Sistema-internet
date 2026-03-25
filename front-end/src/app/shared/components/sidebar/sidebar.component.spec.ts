3aca091960d6f83761b11c84df572608f9c07b8e
Sr-Van <vanderson.dev1@gmail.com>
2026-03-25 16:24:20 -0300

    text(sidebar): add unit test for sidebar component

--- front-end/src/app/shared/components/sidebar/sidebar.component.spec.ts
+++ front-end/src/app/shared/components/sidebar/sidebar.component.spec.ts
@@ -1,5 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
@@ -8,10 +14,10 @@ describe('SidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
-    })
-    .compileComponents();
-    
+      imports: [SidebarComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +26,38 @@ describe('SidebarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have initial state as closed', () => {
+    expect(component.isOpen).toBeFalsy();
+    expect(component.showText).toBeFalsy();
+  });
+
+  it('should toggle sidebar state on toggleSidebar()', () => {
+    component.toggleSidebar();
+    expect(component.isOpen).toBeTrue();
+
+    component.toggleSidebar();
+    expect(component.isOpen).toBeFalse();
+  });
+
+  it('should open sidebar and show text after delay on openSidebar()', fakeAsync(() => {
+    component.openSidebar();
+    expect(component.isOpen).toBeTrue();
+    expect(component.showText).toBeFalse();
+
+    tick(250); // Simulate 250ms delay
+    expect(component.showText).toBeTrue();
+  }));
+
+  it('should close sidebar and hide text immediately, then update isOpen after delay on closeSidebar()', fakeAsync(() => {
+    component.isOpen = true;
+    component.showText = true;
+
+    component.closeSidebar();
+    expect(component.showText).toBeFalse();
+    expect(component.isOpen).toBeTrue();
+
+    tick(250); // Simulate 250ms delay
+    expect(component.isOpen).toBeFalse();
+  }));
 });
