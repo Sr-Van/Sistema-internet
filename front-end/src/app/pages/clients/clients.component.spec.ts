32c0f4f1de0fef8a9adb93621d9e000b3333c052
Sr-Van <vanderson.dev1@gmail.com>
2026-03-27 14:38:03 -0300

    feat(test): testing clients component listGridView toggle working

--- front-end/src/app/pages/clients/clients.component.spec.ts
+++ front-end/src/app/pages/clients/clients.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ClientsComponent } from './clients.component';
 
@@ -8,10 +9,9 @@ describe('ClientsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClientsComponent]
-    })
-    .compileComponents();
-    
+      imports: [ClientsComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ClientsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,32 @@ describe('ClientsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have initial isListView state as false', () => {
+    expect(component.isListView).toBeFalse();
+  });
+
+  it('should initialize with a list of clients', () => {
+    expect(component.clients.length).toBe(3);
+    expect(component.clients[0].name).toBe('John Doe');
+  });
+
+  it('should toggle isListView state when toggleView() is called', () => {
+    expect(component.isListView).toBeFalse();
+    component.toggleView();
+    expect(component.isListView).toBeTrue();
+    component.toggleView();
+    expect(component.isListView).toBeFalse();
+  });
+
+  it('should set isListView correctly when template buttons are clicked', () => {
+    const buttons = fixture.debugElement.queryAll(By.css('button'));
+
+    buttons[1].triggerEventHandler('click', null); // List View button
+    expect(component.isListView).toBeTrue();
+
+    buttons[0].triggerEventHandler('click', null); // Grid View button
+    expect(component.isListView).toBeFalse();
   });
 });
