aec21aa665091ce84d24ff0803d859f46fd72fb7
Vanderson Erlan <vanderson.dev1@gmail.com>
2026-04-21 21:31:18 -0300

    refactor: migrate API from Flask to FastAPI

--- back-end/app.py
+++ back-end/app.py
@@ -1,30 +1,28 @@
 import sqlite3
-from flask import Flask, request, jsonify,g
+from fastapi import FastAPI, Depends
+from typing import List
+import uvicorn
 from model.model import Database
 from controller.controller import Controller
 
-app = Flask(__name__)
+app = FastAPI()
 DATABASE = 'clients.db'
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
+    db = sqlite3.connect(DATABASE)
+    try:
+        yield db
+    finally:
         db.close()
 
-@app.route('/api', methods=['GET'])
-def helloworld():
-    db_wrapper = Database(get_db())
+@app.get("/api")
+def helloworld(db: sqlite3.Connection = Depends(get_db)):
+    db_wrapper = Database(db)
     controller = Controller(db_wrapper)
+    
     orders = controller.get_all_orders()
-    return jsonify(orders)
+    return "Hello World! Here are the service orders: " + str(orders)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
     print("working")
